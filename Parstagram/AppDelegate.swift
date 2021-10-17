4e77e3deb71b32f6044d7316c51e0bc06fda4c3d
Lauren Work <workla@oregonstate.edu>
2021-10-16 23:38:25 -0700

    Initial work for login flow and get picture in picture screen

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //parse addition
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "5QZbWqi8yclLSBXPbgY5sEjiIgU8xdzn5yOiYpwb" // <- UPDATE
+                    $0.clientKey = "cl4rbvz7oEjopUyVxVe4ajE7icPRzU6EcY5Dw8gG" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
