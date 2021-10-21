1c1f505e31e9194f5e635fa42b1ef1cc4110cc6c
Lauren Work <workla@oregonstate.edu>
2021-10-20 21:57:02 -0700

    Save pics, make tableview, and pull to refresh and infinite scroll

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -15,6 +16,23 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        post["image"] = file
+        
+        
+        post.saveInBackground { success, error in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("Success")
+            } else {
+                print("Error")
+            }
+        }
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -6,24 +6,92 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [PFObject]()
+    var numberOfPosts: Int!
+    var myRefreshControl: UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        myRefreshControl = UIRefreshControl()
+        myRefreshControl.addTarget(self, action: #selector(loadPosts), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //refreshing table
+        loadPosts()
+    }
+    
+    @objc func loadPosts(){
+        numberOfPosts = 5
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts
+        query.order(byDescending: "createdAt")
+        query.findObjectsInBackground { postsReturned, error in
+            if postsReturned != nil {
+                self.posts = postsReturned!
+                self.tableView.reloadData()
+                self.myRefreshControl.endRefreshing()
+            }
+        }
+    }
+    
+    func loadMorePosts(){
+        numberOfPosts += 5
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts
+        query.order(byDescending: "createdAt")
+        query.findObjectsInBackground { postsReturned, error in
+            if postsReturned != nil {
+                self.posts = postsReturned!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == posts.count {
+            loadMorePosts()
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+
+
+        let user = post["author"] as! PFUser
+        cell.authorLabel.text = user.username
+
+        cell.captionLabel.text = post["caption"] as! String
+
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+
+        cell.photoView.af.setImage(withURL: url)
+
+        return cell
+        
+    }
 
 }
